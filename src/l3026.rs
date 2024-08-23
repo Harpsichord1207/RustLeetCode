b69f1f8917da3eb341b963cbc4b9d7a3ef204a63
LiuTao <tao1109.liu@samsung.com>
2024-08-23 16:07:47 +0800

    3026

---
+++ src/l3026.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn maximum_subarray_sum(nums: Vec<i32>, k: i32) -> i64 {
+        let mut result = i64::MIN;
+        let mut current_sum = 0i64;
+
+        let nums: Vec<i64> = nums.into_iter().map(|n| n as i64).collect();
+        let k = k as i64;
+
+        let mut sum_map = HashMap::new();
+
+        for num in nums {
+            if sum_map.get(&num).unwrap_or(&i64::MAX) > &current_sum {
+                sum_map.insert(num, current_sum);
+            }
+            current_sum += num;
+
+            for key in [num + k, num - k] {
+                if sum_map.contains_key(&key) {
+                    result = result.max(current_sum - sum_map.get(&key).unwrap());
+                }
+            }
+        }
+
+        if result == i64::MIN {
+            0
+        } else {
+            result
+        }
+    }
+}
